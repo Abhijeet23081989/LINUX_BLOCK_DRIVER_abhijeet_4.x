3e12e2d18836e43d327a3d1b94d1d76ee7b92af0
abhijeet.srivastava <abhijeet.srivastava2308@gmail.com>
2020-01-12 21:11:14 +0530

    added create_block_device

--- blk_drv.c
+++ blk_drv.c
@@ -1,19 +1,40 @@
 #include<linux/fs.h>
 #include<linux/module.h>
 #include<linux/init.h>
+#include<linux/genhd.h>
 #define MY_BLOCK_MAJOR 240
 #define My_BLKDEV_NAME "BLOCKADE"
+
+//++++++++++++Block device structure to store important elements describing the device+++++++++
+
+static struct my_blk_dv
+{
+	struct gendisk *gd;
+}dev;
+
+//+++++++++++++++++++++++++++++++++++++++++++
 
 static int my_block_init(void)
 {
 	int status;
+	//#1================REGISTER BLOCK DRIVER====================
+	
 	status = register_blkdev(MY_BLOCK_MAJOR,My_BLKDEV_NAME);
 	if(status < 0){
 		printk(KERN_ERR"Block Device not registering\n");
 	}
+
+	//#2===============CREATE A BLOCK DEVICE====================
+		
+	create_block_device();
 	
  return 0;	
 }module_init(my_block_init);
+
+static int create_block_device()
+{
+
+}
 
 static void my_block_exit(void)
 {
