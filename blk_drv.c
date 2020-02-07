f00e2caa971bff0f47e74339d9864fe96fedcc5c
abhijeet.srivastava <abhijeet.srivastava@inventum.net>
2020-02-07 18:17:54 +0530

    07022020 --> added blk_queue_logical_block_size() and some more explanations in comment. blk_queue_logical_block_size() causing compile error in linux-4.14 will check at home

--- blk_drv.c
+++ blk_drv.c
@@ -1,7 +1,8 @@
-#include<linux/fs.h>
+#include<linux/blkdev.h>
 #include<linux/module.h>
 #include<linux/init.h>
 #include<linux/genhd.h>
+#include<linux/fs.h>
 
 #define MY_BLOCK_MAJOR 0 //default block number should be zero so that the kernal can allot the major number
 #define MY_BLK_MNR 1
@@ -13,8 +14,9 @@
 
 static struct my_blk_dv
 {
+	spinlock_t lockdown;//spin-lock is applied for mutual exclusion (So that the block device driver can be accesssed by one user at a time)
 	struct gendisk *gd;//struct gendisk is the basic structure in working with block devices
-	struct request_queue *que;//?
+	struct request_queue *que;//The device request queue
 }dev;
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -24,19 +26,53 @@ static struct my_blk_dv
 struct block_device_operations{ 
 }my_blk_fops;
 
-//*****************Declaration*********************
-
-static int create_block_device(struct my_blk_dv*);
-static int del_blk_dv(struct my_blk_dv*);
-
-//*************************************************
-
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 int status;//status is to hold Major number and also the failed status of register_blkdev()
 int count;//everytime open operation is called count goes +1 ,count goes -1 for every user who have called open operation and when count =0 del_gendisk() is called
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+static int create_block_device(struct my_blk_dv *ptr_dev)
+{
+	/********Allocating Disk****************/
+	ptr_dev->gd=alloc_disk(MY_BLK_MNR);
+	if(ptr_dev->gd){
+		printk(KERN_NOTICE"alloc_disk failed!!\n");
+		return -ENOMEM;//ENOMEM=?? and why??	
+	}
+	/***************************************/
+
+	/*******Initializing struct gendisk*****/
+	ptr_dev->gd->major=status;
+	ptr_dev->gd->first_minor=0;
+	ptr_dev->gd->fops=&my_blk_fops;
+	ptr_dev->gd->queue=ptr_dev->que;
+	ptr_dev->gd->private_data=ptr_dev;
+	snprintf(ptr_dev->gd->disk_name,32,"abd");//?
+	set_capacity(ptr_dev->gd,NR_SECTORS);//?
+	/***************************************/
+	
+	/******************************************/
+	/*To inform the kernel about the device sector size, 
+	 *a parameter of the request queue must be set just 
+	 *after the request queue is allocated, using the 
+	 *blk_queue_logical_block_size() function*/
+
+	blk_queue_logical_block_size(ptr_dev->que, 512);
+        /******************************************/
+
+	/*********Adding Disk to the system*****/
+
+	/*Note that immediately after calling the add_disk() function (actually even during the call), 
+	 * the disk is active and its methods can be called at any time. 
+	 * As a result, this function SHOULD NOT BE CALLED before the driver is 
+	 * fully initialized and ready to respond to requests for the registered disk.*/
+	add_disk(ptr_dev->gd);
+	/***************************************/
+	return 0;
+}
+
 static int my_block_init(void)
 {
 	//#1================REGISTER BLOCK DRIVER====================
@@ -55,34 +91,6 @@ static int my_block_init(void)
  return 0;	
 }module_init(my_block_init);
 
-static int create_block_device(struct my_blk_dv *ptr_dev)
-{
-	/********Allocating Disk****************/
-	ptr_dev->gd=alloc_disk(MY_BLK_MNR);
-
-	/***************************************/
-
-	/*******Initializing struct gendisk*****/
-	ptr_dev->gd->major=status;
-	ptr_dev->gd->first_minor=0;
-	ptr_dev->gd->fops=&my_blk_fops;
-	ptr_dev->gd->queue=ptr_dev->que;
-	ptr_dev->gd->private_data=ptr_dev;
-	snprintf(ptr_dev->gd->disk_name,32,"abd");//?
-	set_capacity(ptr_dev->gd,NR_SECTORS);//?
-	/***************************************/
-
-	/*********Adding Disk to the system*****/
-
-	/*Note that immediately after calling the add_disk() function (actually even during the call), 
-	 * the disk is active and its methods can be called at any time. 
-	 * As a result, this function SHOULD NOT BE CALLED before the driver is 
-	 * fully initialized and ready to respond to requests for the registered disk.*/
-	add_disk(ptr_dev->gd);
-	/***************************************/
-	return 0;
-}
-
 static int del_blk_dv(struct my_blk_dv *ptr_dev)
 {
 
@@ -91,6 +99,7 @@ static int del_blk_dv(struct my_blk_dv *ptr_dev)
 		del_gendisk(ptr_dev->gd);
 	return 0;
 }
+
 static void my_block_exit(void)
 {
 	unregister_blkdev(status,My_BLKDEV_NAME);
@@ -109,3 +118,4 @@ static void my_block_exit(void)
 	 *no users left of the device*/
 
 }module_exit(my_block_exit);
+
